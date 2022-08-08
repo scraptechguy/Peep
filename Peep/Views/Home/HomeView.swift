8b1fa6576c0af894a59ca8e05c69273c84b2a7e5
Rostislav Brož <scraptechguy@gmail.com>
2022-08-08 21:19:44 +0200

    Create top of the home view UI

--- Peep/Views/Home/HomeView.swift
+++ Peep/Views/Home/HomeView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State var search = ""
+    
     var body: some View {
-        Map()
-            .ignoresSafeArea()
+        ZStack {
+            Map()
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    TextField("Search the globe", text: $search)
+                        .padding(.all, 20.0)
+                        .frame(width: 250)
+                        .background(
+                            Rectangle()
+                                .fill(.ultraThinMaterial)
+                        )
+                        .mask(
+                            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        )
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(.thinMaterial)
+                                .frame(width: 60, height: 60)
+                                .mask(
+                                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                                )
+                            
+                            Image(systemName: "gear")
+                                .foregroundColor(.white)
+                        }
+                    }).padding(.leading, 10)
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.dark)
     }
 }
