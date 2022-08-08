aa6299c0e718c7f62ecc7093ade6466eca6049ce
Rostislav Brož <scraptechguy@gmail.com>
2022-08-08 12:49:29 +0200

    Initial Commit

---
+++ Peep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 8/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peep/PeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeepApp.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
