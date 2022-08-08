aae7d9f8acec4da95b42ab88ffe140ba107bd4f6
Rostislav Brož <scraptechguy@gmail.com>
2022-08-08 14:40:22 +0200

    Create basic structure

---
+++ Peep/ViewModels/ContentModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentModel.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 8/8/22.
+//
+
+import Foundation
+import SwiftUI
+
+class ContentModel {
+    
+}
---
+++ Peep/Views/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 8/8/22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
