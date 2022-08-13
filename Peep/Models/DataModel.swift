4f2520046dc4ddb0eb473391ccb1cee8d754b5c9
Rostislav Brož <scraptechguy@gmail.com>
2022-08-13 18:20:33 +0200

    Process additional JSON information

--- Peep/Models/DataModel.swift
+++ Peep/Models/DataModel.swift
@@ -11,7 +11,18 @@ struct DataModel: Codable, Identifiable {
     
     var id: String?
     var adresa: String?
+    var umisteni: String?
     var zdelka: String?
     var zsirka: String?
+    var thodin: String?
+    var tukazatel: String?
+    var tciselnik: String?
+    var azimut: String?
+    var dnodus: String?
+    var cislice: String?
+    var vznik: String?
+    var stav: String?
+    var evc: String?
+    var obrazky: String?
 
 }
