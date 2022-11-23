ab77e17c77dbd70ff4bf8ea640b3f5eba0403d8f
Rostislav Brož <scraptechguy@gmail.com>
2022-11-23 21:06:50 +0100

    Update ExplanationItem.swift

--- Peep/Helpers/ExplanationItem.swift
+++ Peep/Helpers/ExplanationItem.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ExplanationItem: View {
     
+    let screenSize: CGRect = UIScreen.main.bounds
+    
     let item: String
     let explanation: String
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            
+            Text(item)
+                .bold()
+                .font(.title)
+                .frame(width: 35, height: 35)
+                .minimumScaleFactor(0.1)
+            
+            Spacer()
+            
+            Text(explanation)
+                .frame(width: screenSize.width / 1.7)
+            
+            Spacer()
+        }.foregroundColor(Color("Font"))
     }
 }
 
 struct ExplanationItem_Previews: PreviewProvider {
     static var previews: some View {
-        ExplanationItem(item: "S", explanation: "")
+        ExplanationItem(item: "S", explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
     }
 }
