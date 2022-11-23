9491017a3cb65ee28cd14cdef9e3dfaa2ca88a47
Rostislav Brož <scraptechguy@gmail.com>
2022-11-23 20:44:24 +0100

    Create ExplanationItem.swift

---
+++ Peep/Helpers/ExplanationItem.swift
@@ -0,0 +1,24 @@
+//
+//  ExplanationItem.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 11/23/22.
+//
+
+import SwiftUI
+
+struct ExplanationItem: View {
+    
+    let item: String
+    let explanation: String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExplanationItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ExplanationItem(item: "S", explanation: "")
+    }
+}
