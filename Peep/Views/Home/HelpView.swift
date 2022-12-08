eb280d99e0c5c49b4df11ae906e98d85f999e54d
Rostislav Brož <scraptechguy@gmail.com>
2022-12-08 13:20:48 +0100

    Create HelpView.swift

---
+++ Peep/Views/Home/HelpView.swift
@@ -0,0 +1,20 @@
+//
+//  HelpView.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 12/8/22.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HelpView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelpView()
+    }
+}
