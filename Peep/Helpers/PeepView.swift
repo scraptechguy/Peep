6914b4e945047e4dddbe0a462f272512008a1027
Rostislav Brož <scraptechguy@gmail.com>
2022-12-09 23:41:19 +0100

    Create something

---
+++ Peep/Helpers/PeepView.swift
@@ -0,0 +1,20 @@
+//
+//  PeepView.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 12/9/22.
+//
+
+import SwiftUI
+
+struct PeepView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PeepView_Previews: PreviewProvider {
+    static var previews: some View {
+        PeepView()
+    }
+}
