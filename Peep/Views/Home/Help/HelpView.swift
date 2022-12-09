80132eb7f7059f575ee4507ab0905a76a5bc18cc
Rostislav Brož <scraptechguy@gmail.com>
2022-12-09 18:55:35 +0100

    Change help view design once again

--- Peep/Views/Home/Help/HelpView.swift
+++ Peep/Views/Home/Help/HelpView.swift
@@ -14,6 +14,8 @@ struct HelpView: View {
     @State private var index: Int = 0
     @State private var screenIsLast = false
     
+    let screenSize: CGRect = UIScreen.main.bounds
+    
     let detailGalleryGuide: LocalizedStringKey = "detailGalleryGuide"
     
     var body: some View {
@@ -23,23 +25,42 @@ struct HelpView: View {
             
             VStack {
                 TabView(selection: $index) {
-                    Image("Help1")
-                        .resizable()
-                        .scaledToFit()
-                        .tag(0)
-                    
-                    Image("Help2")
-                        .resizable()
-                        .scaledToFit()
-                        .tag(1)
-                    
-                    Image("dangerous_initial")
-                        .tag(2)
+                    ForEach(guides.indices, id: \.self) { i in
+                        GeometryReader { proxy in
+                            let size = proxy.size
+                            
+                            VStack {
+                                VStack {
+                                    guideScreen(size: size, index: i)
+                                }.rotation3DEffect(.degrees(proxy.frame(in: .global).minX / -10), axis: (x: 0, y: 1, z: 0))
+                                    .shadow(color: Color.gray.opacity(0.3), radius: 10, x: 0, y: 10)
+                                    .blur(radius: abs(proxy.frame(in: .global).minX / 40))
+                            }.frame(width: screenSize.width, height: screenSize.height / 1.5, alignment: .center)
+                        }.tag(i)
+                    }
                 }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
                     .frame(maxHeight: .infinity)
                     .ignoresSafeArea()
             }
         }.preferredColorScheme(model.isLightMode ? .dark : .light)
+    }
+    
+    @ViewBuilder
+    func guideScreen(size: CGSize, index: Int) -> some View {
+        let guide = guides[index]
+        
+        VStack {
+            Image(guide.imageName)
+                .resizable()
+                .scaledToFit()
+            
+            Text(guide.title)
+                .font(.title2.bold())
+            
+            Text(guide.text)
+                .multilineTextAlignment(.center)
+                .padding()
+        }
     }
 }
 
