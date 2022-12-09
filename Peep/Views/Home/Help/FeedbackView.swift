d7b103bd69d488931193a188649efe284e857592
Rostislav Brož <scraptechguy@gmail.com>
2022-12-09 18:56:19 +0100

    Create FeedbackView.swift

---
+++ Peep/Views/Home/Help/FeedbackView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedbackView.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 12/9/22.
+//
+
+import SwiftUI
+
+struct FeedbackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedbackView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedbackView()
+    }
+}
