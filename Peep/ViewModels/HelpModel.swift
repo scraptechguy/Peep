e2fef7ab64d51f52ec4e5174f29ef13cc884df3c
Rostislav Brož <scraptechguy@gmail.com>
2022-12-11 14:44:35 +0100

    Add 8 more `.init()`s

--- Peep/ViewModels/HelpModel.swift
+++ Peep/ViewModels/HelpModel.swift
@@ -20,5 +20,13 @@ var guides: [Help] = [
 
     .init(imageName: "Help1", title: String(localized: "onboardingHeading1"), text: String(localized: "onboardingText1")),
     .init(imageName: "Help2", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help3", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help4", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help5", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help6", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help7", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help8", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help9", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
+    .init(imageName: "Help10", title: String(localized: "onboardingHeading2"), text: String(localized: "onboardingText2")),
 
 ]
