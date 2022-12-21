a367dd613de94d6170ac0ec9fca3a79b1e9919ae
Rostislav Brož <scraptechguy@gmail.com>
2022-12-21 14:38:05 +0100

    Create Compass.swift

---
+++ Peep/Views/Home/Detail/Compass.swift
@@ -0,0 +1,20 @@
+//
+//  Compass.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 12/21/22.
+//
+
+import SwiftUI
+
+struct Compass: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Compass_Previews: PreviewProvider {
+    static var previews: some View {
+        Compass()
+    }
+}
