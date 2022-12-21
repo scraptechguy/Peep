48a65fade688091ca297884a8718fda81864f761
Rostislav Brož <scraptechguy@gmail.com>
2022-12-21 15:37:07 +0100

    Update Compass.swift

--- Peep/Views/Home/Detail/Compass/Compass.swift
+++ Peep/Views/Home/Detail/Compass/Compass.swift
@@ -11,51 +11,53 @@ struct Compass: View {
     
     @ObservedObject var compassHeading = CompassHeading()
     
+    let screenSize: CGRect = UIScreen.main.bounds
+    
     var body: some View {
         VStack {
             ZStack {
                 ForEach(Marker.markers(), id: \.self) { marker in
-                    CompassMarkerView(marker: marker, compassDegress: 0)
+                    CompassMarkerView(marker: marker, compassDegress: self.compassHeading.degrees)
                 }
-            }
-            .frame(width: 300,
-                   height: 300)
-            .rotationEffect(Angle(degrees: self.compassHeading.degrees))
-            .statusBar(hidden: true)
+            }.frame(width: screenSize.width / 2.5, height: screenSize.width / 2.5)
+                .rotationEffect(Angle(degrees: self.compassHeading.degrees))
+                .statusBar(hidden: true)
         }
     }
 }
 
 
 struct CompassMarkerView: View {
+    
+    @ObservedObject var compassHeading = CompassHeading()
+    
     let marker: Marker
     let compassDegress: Double
 
     var body: some View {
         VStack {
             Capsule()
-                    .frame(width: self.capsuleWidth(),
-                            height: self.capsuleHeight())
-                    .foregroundColor(self.capsuleColor())
-                    .padding(.bottom, 120)
+                .frame(width: self.capsuleWidth(), height: self.capsuleHeight())
+                .foregroundColor(self.capsuleColor())
 
             Text(marker.label)
                     .fontWeight(.bold)
+                    .foregroundColor(.primary)
                     .rotationEffect(self.textAngle())
                     .padding(.bottom, 80)
         }.rotationEffect(Angle(degrees: marker.degrees))
     }
     
     private func capsuleWidth() -> CGFloat {
-        return marker.degrees == 0 ? 7 : 3
+        return marker.degrees == 0 ? 4 : 1
     }
 
     private func capsuleHeight() -> CGFloat {
-        return marker.degrees == 0 ? 45 : 30
+        return marker.degrees == 0 ? 22 : 12
     }
 
     private func capsuleColor() -> Color {
-        return marker.degrees == 0 ? .primary : .primary
+        return marker.degrees == 0 ? .green : .primary
     }
 
     private func textAngle() -> Angle {
@@ -76,17 +78,29 @@ struct Marker: Hashable {
     static func markers() -> [Marker] {
         return [
             Marker(degrees: 0, label: "N"),
+            Marker(degrees: 15),
             Marker(degrees: 30),
+            Marker(degrees: 45),
             Marker(degrees: 60),
+            Marker(degrees: 75),
             Marker(degrees: 90, label: "E"),
+            Marker(degrees: 105),
             Marker(degrees: 120),
+            Marker(degrees: 135),
             Marker(degrees: 150),
+            Marker(degrees: 165),
             Marker(degrees: 180, label: "S"),
+            Marker(degrees: 195),
             Marker(degrees: 210),
+            Marker(degrees: 225),
             Marker(degrees: 240),
+            Marker(degrees: 255),
             Marker(degrees: 270, label: "w"),
+            Marker(degrees: 285),
             Marker(degrees: 300),
-            Marker(degrees: 330)
+            Marker(degrees: 315),
+            Marker(degrees: 330),
+            Marker(degrees: 345)
         ]
     }
 }
