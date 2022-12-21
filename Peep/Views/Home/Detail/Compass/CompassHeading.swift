bf05e61ee8e8b76aac2e3a90d9a459ca31102a3f
Rostislav Brož <scraptechguy@gmail.com>
2022-12-21 14:53:27 +0100

    Update CompassHeading.swift

--- Peep/Views/Home/Detail/Compass/CompassHeading.swift
+++ Peep/Views/Home/Detail/Compass/CompassHeading.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Combine
+import CoreLocation
+
+class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var degrees: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingLocation()
+            self.locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.degrees = -1 * newHeading.magneticHeading
+    }
+}
