792b7bc360c338648baec95916361f5feef7240a
Rostislav Brož <scraptechguy@gmail.com>
2023-01-04 12:23:39 +0100

    Create AppIconView.swift

---
+++ Peep/Views/Home/AppIconView.swift
@@ -0,0 +1,20 @@
+//
+//  AppIconView.swift
+//  Peep
+//
+//  Created by Rostislav Brož on 1/4/23.
+//
+
+import SwiftUI
+
+struct AppIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppIconView()
+    }
+}
